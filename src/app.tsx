20ef6a243b0063cebd578932da156d345e64598d
masudaS <masuda@infocity.co.jp>
2021-03-12 15:53:51 +0900

    中心の座標と拡大率の変更に対応

--- src/app.tsx
+++ src/app.tsx
@@ -23,9 +23,9 @@ const App = () => {
     <div>
       <canvas width={Canvas.Width} height={Canvas.Height} ref={canvasRef}></canvas>
       <br />
-      中心のx座標:<input type="number" value={x}></input>
-      中心のy座標:<input type="number" value={y}></input>
-      拡大率:<input type="number" value={expansionRate}></input>
+      中心のx座標:<input type="number" value={x} onInput={e => setState({ x:  Number.parseFloat((e.target as HTMLInputElement).value), y, expansionRate })}></input>
+      中心のy座標:<input type="number" value={y} onInput={e => setState({ y:  Number.parseFloat((e.target as HTMLInputElement).value), x, expansionRate })} ></input>
+      拡大率:<input type="number" value={expansionRate} onInput={e => setState({ expansionRate:  Number.parseFloat((e.target as HTMLInputElement).value), x, y })}></input>
       <button onClick={onClickUpdate}>更新</button>
     </div>
   )
