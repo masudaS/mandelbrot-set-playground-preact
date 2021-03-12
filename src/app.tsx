9db6273dcafacdeedac3de67e64f3de37d62ed26
masudaS <masuda@infocity.co.jp>
2021-03-12 15:24:26 +0900

    とりあえずガワだけ作った

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,22 @@
 import { render } from "preact";
 
+const Canvas = {
+  Width: 800,
+  Height: 800
+}
+
+const App = () => (
+  <div>
+    <canvas width={Canvas.Width} height={Canvas.Height}></canvas>
+    <br />
+    中心のx座標:<input type="number" value={0}></input>
+    中心のy座標:<input type="number" value={0}></input>
+    拡大率:<input  type="number" value={400}></input>
+    <button>更新</button>
+  </div>
+)
+
 render(
-  <div>Hello Preact!</div>,
+  <App />,
   document.getElementById("app")!
 )
